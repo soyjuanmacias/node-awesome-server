1c85272728eb04559c85e4e1420a60491c6cb5ca
Juan Macías <info@juanmacias.es>
2023-01-11 06:29:16 +0100

    config refactor index routes

--- src/api/routes/root.routes.js
+++ src/api/routes/root.routes.js
@@ -4,11 +4,9 @@ import { Router } from 'express';
  * Rutas sueltas y huérfanas
  */
 const rootRoutes = router => {
-
   router.get('/', (req, res, next) => res.send('Server working route /'));
-  router.head('/status', (req, res, next) => res.status(200).json('HEAD /status working'));
   router.get('/status', (req, res, next) => res.status(200).json('Server is Online'));
-
+  router.head('/status', (req, res, next) => res.status(200).json('HEAD /status working'));
   return router;
 };
 
