e7f0ce7ac4e70b0f48619201bfa247bb892b16a5
Juan Macías <info@juanmacias.es>
2023-01-11 06:32:33 +0100

    config add data layer

---
+++ src/domain/db/user.db.js
@@ -0,0 +1,44 @@
+import { User } from '../models/index.js';
+import Logger from '../../loaders/logger.js';
+
+/**
+ * Capa de datos.
+ * 
+ * Encargada de la comunicación con la base de datos
+ * En esta capa se implementan los métodos de acceso a la base de datos
+ * para cada entidad.
+ * 
+ * Esta de datos capa no debe tener ninguna dependencia con el framework.
+ * Si se decidiera cambiar de base de datos, con re-implementar esta capa
+ * la aplicación entera seguiría funcionando con normalidad y sin necesidad de
+ * modificar ninguna otra capa.
+ */
+
+export const findUserById = async id => {
+  try {
+    return await User.findById(id).lean();
+  } catch (error) {
+    Logger.error('Error obteniendo el usuario', id);
+    return null;
+  }
+}
+
+export const findUserByEmail = async email => {
+  try {
+    return await User.findOne({ email }).select('+password').lean();
+  } catch (error) {
+    Logger.error('Error obteniendo el usuario', email);
+    return error;
+  }
+};
+
+export const createUser = async user => {
+  try {
+    const newUser = new User(user);
+    const created = await newUser.save();
+    return created.toJSON();
+  } catch (error) {
+    Logger.error('Error creando el usuario %o', error);
+    return error;
+  }
+};
